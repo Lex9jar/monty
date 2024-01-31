0b283b38ccfc70cffb0299a29aca0ac10081ce18
Lex9jar <alexanderedim89@gmail.com>
2024-02-01 00:51:28 +0100

    A monty ByteCode Interpreter

---
+++ main.c
@@ -0,0 +1,43 @@
+#include "monty.h"
+
+bus_t bus = {0, NULL, NULL, NULL, {NULL, NULL}};
+
+/**
+ * main - An interpreter for Monty ByteCodes files.
+ * @argc: Number of arguments passed
+ * @argv: Array of arguments passed
+ *
+ * Return: 0 upon Success.
+ */
+int main(int argc, char *argv[])
+{
+	ssize_t r = 1;
+	size_t n = 0;
+
+	if (argc != 2)
+	{
+		fprintf(stderr, "USAGE: monty file");
+		exit(EXIT_FAILURE);
+	}
+
+	bus.monty_file = fopen(argv[1], "r");
+	if (bus.monty_file == NULL)
+	{
+		fprintf(stderr, "Error: Can't open file %s\n", argv[1]);
+		exit(EXIT_FAILURE);
+	}
+
+	while (r > 0)
+	{
+		bus.line_number++;
+		bus.lineptr = NULL;
+		r = getline(&(bus.lineptr), &n, bus.monty_file);
+		if (r > 0)
+			get_opcodes();
+		free(bus.lineptr);
+	}
+	free_stack(&(bus.stack));
+	fclose(bus.monty_file);
+
+	return (0);
+}
