bb660305c8c7841bd05ecb065474b6fb34240221
Lex9jar <alexanderedim89@gmail.com>
2024-02-01 16:55:46 +0100

    Execute monty commands

--- execute_opcode.c
+++ execute_opcode.c
@@ -10,7 +10,7 @@ void get_opcodes(void)
 {
 	char *commands[] = {"push", "pall", "pint",
 			"pop", "swap", "add",
-			NULL};
+			"nop", NULL};
 	int i, flg = 0;
 
 	bus.opcode = strtok(bus.lineptr, " \t\n");
@@ -42,7 +42,7 @@ void execute(void)
 	instruction_t fetch[] = {
 		{"push", push},	{"pall", pall}, {"pint", pint},
 		{"pop", pop}, {"swap", swap}, {"add", add},
-		{NULL, NULL}
+		{"nop", nop}, {NULL, NULL}
 	};
 	int i = 0;
 
