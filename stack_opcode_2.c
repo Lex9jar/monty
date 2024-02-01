712ef5d14c033573830e83d6377278814733f430
Lex9jar <alexanderedim89@gmail.com>
2024-02-01 17:05:10 +0100

    More monty command functions

--- stack_opcode_2.c
+++ stack_opcode_2.c
@@ -35,3 +35,27 @@ void nop(stack_t **stack, unsigned int line_number)
 	(void)stack;
 	(void)line_number;
 }
+
+/**
+ * sub - substracts the top element of the stack from the second
+ * top element of the stack
+ * @stack: A doubly linked list
+ * @line_number: Command line number in monty file
+ *
+ * Return: void.
+ */
+void sub(stack_t **stack, unsigned int line_number)
+{
+	int dif;
+
+	if (bus.size < 2)
+	{
+		fprintf(stderr, "L%u: can't sub, stack too short\n", line_number);
+		free_bus();
+		exit(EXIT_FAILURE);
+	}
+
+	dif = (*stack)->n - (*stack)->next->n;
+	pop(stack, line_number);
+	(*stack)->n = dif;
+}
