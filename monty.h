07ef9e58dd507097f27a2053fd36ea6b1c941114
Lex9jar <alexanderedim89@gmail.com>
2024-02-01 17:56:00 +0100

    Header file

--- monty.h
+++ monty.h
@@ -77,6 +77,7 @@ void _add(stack_t **stack, unsigned int line_number);
 void _nop(stack_t **stack, unsigned int line_nnumber);
 void _sub(stack_t **stack, unsigned int line_number);
 void _div(stack_t **stack, unsigned int line_number);
+void _mul(stack_t **stack, unsigned int line_number);
 
 /* = > = > = > HANDY_UTILS < = < = < = */
 int not_number(char *token);
