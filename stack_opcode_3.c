3e2c1b7c4ff545a33e3647832457a60824869a45
Lex9jar <alexanderedim89@gmail.com>
2024-02-01 18:25:37 +0100

    COMMANDS: _mod & _pchar

--- stack_opcode_3.c
+++ stack_opcode_3.c
@@ -30,3 +30,32 @@ void _mod(stack_t **stack, unsigned int line_number)
 	_pop(stack, line_number);
 	(*stack)->n = product;
 }
+
+/**
+ * _pchar - Prints the char at the top of the stack, followed by a new line
+ * @stack: pointer to a stack
+ * @line_number: current command line number in monty_file
+ *
+ * Return: void.
+ */
+void _pchar(stack_t **stack, unsigned int line_number)
+{
+	int chr;
+
+	if (*stack == NULL)
+	{
+		fprintf(stderr, "L%u: can't pchar, stack empty\n", line_number);
+		free_bus();
+		exit(EXIT_FAILURE);
+	}
+
+	chr = (*stack)->n;
+	if (chr < 0 || chr > 127)
+	{
+		fprintf(stderr, "L%u: can't pchar, value out of range\n", line_number);
+		free_bus();
+		exit(EXIT_FAILURE);
+	}
+
+	write(1, &chr, 1);
+}
