d1d7e0faf69461a5aaad94bd78f5c113a05047f2
Lex9jar <alexanderedim89@gmail.com>
2024-02-01 17:06:39 +0100

    More monty command functions

--- stack_opcode_2.c
+++ stack_opcode_2.c
@@ -55,7 +55,7 @@ void sub(stack_t **stack, unsigned int line_number)
 		exit(EXIT_FAILURE);
 	}
 
-	dif = (*stack)->n - (*stack)->next->n;
+	dif = (*stack)->next->n - (*stack)->n;
 	pop(stack, line_number);
 	(*stack)->n = dif;
 }
