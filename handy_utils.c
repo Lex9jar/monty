0a8fac3d99caedd45661ec26d6058d0eeb3a53cf
Lex9jar <alexanderedim89@gmail.com>
2024-02-02 14:44:54 +0100

    Handy functions

--- handy_utils.c
+++ handy_utils.c
@@ -21,3 +21,67 @@ int not_number(char *token)
 
 	return (0);
 }
+
+/**
+ * add_at_beg - Adds a new node at the beginning of a stack
+ *
+ * Return: void.
+ */
+void add_at_beg(void)
+{
+	stack_t *new;
+
+	new = malloc(sizeof(stack_t));
+	if (new == NULL)
+	{
+		fprintf(stderr, "Error: malloc failed\n");
+		free_bus();
+		exit(EXIT_FAILURE);
+	}
+
+	new->prev = NULL;
+	new->n = atoi(bus.arg);
+
+	if (bus.stack == NULL)
+		new->next = NULL;
+	else
+		new->next = bus.stack;
+
+	bus.stack = new;
+	bus.size++;
+}
+
+/**
+ * add_at_end - Adds a new node at the end of a stack
+ *
+ * Return: void.
+ */
+void add_at_end(void)
+{
+	stack_t *new, *ptr = bus.stack;
+
+	new = malloc(sizeof(stack_t));
+	if (new == NULL)
+	{
+		fprintf(stderr, "Error: malloc failed\n");
+		free_bus();
+		exit(EXIT_FAILURE);
+	}
+
+	new->n = atoi(bus.arg);
+	new->next = NULL;
+
+	if (bus.stack == NULL)
+	{
+		new->prev = NULL;
+		bus.stack = new;
+	}
+	else
+	{
+		while (ptr->next)
+			ptr = ptr->next;
+		ptr->next = new;
+		new->prev = ptr;
+	}
+	bus.size++;
+}
