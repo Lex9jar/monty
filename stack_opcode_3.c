8f8333b8bf7a8a22aafd66c1700b161dcff72803
Lex9jar <alexanderedim89@gmail.com>
2024-02-02 11:39:15 +0100

    COMMANDS: _mod, _pchar, _pstr & _rotl

--- stack_opcode_3.c
+++ stack_opcode_3.c
@@ -84,3 +84,30 @@ void _pstr(stack_t **stack, unsigned int line_number)
 	}
 	printf("\n");
 }
+
+/**
+ * _rotl - Rotates the stack to the top
+ * @stack: pointer to a stack
+ * @line_number: current command line number in monty_file
+ *
+ * Return: void.
+ */
+void _rotl(stack_t **stack, unsigned int line_number)
+{
+	stack_t *ptr, *bottom = *stack;
+
+	(void)line_number;
+	if (*stack == NULL || bus.size == 1)
+		return;
+
+	ptr = *stack;
+	while (ptr->next)
+		ptr = ptr->next;
+
+	*stack = (*stack)->next;
+	(*stack)->prev = NULL;
+
+	ptr->next = bottom;
+	bottom->next = NULL;
+	bottom->prev = ptr;
+}
